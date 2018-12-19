c6a16254bf8496c95d7f45202e5b9c633b24720c
Ganesh Londhe <ganeshlondhe2020@gmail.com>
2018-12-19 16:05:57 +0530

    limits the rows, offset starting point of the rows

---
+++ queries/limit_offset.py
@@ -0,0 +1,14 @@
+#limits the query to the "limit number" rows
+#offset is the starting point of the rows
+
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost",user="root",passwd="shA2189",database="world")
+
+mycursor = mydb.cursor()
+
+sql = "select * from customers limit 3 offset 2"
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+for x in myresult:
+	print(x)

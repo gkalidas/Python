d1e08d6af6a4cf8f99e2243c3460e8c72736c0e9
Ganesh Londhe <ganeshlondhe2020@gmail.com>
2018-12-28 11:06:55 +0530

    removes vowels from the string

---
+++ practice/anti_vowel.py
@@ -0,0 +1,10 @@
+#delete vowels from string
+
+def anti_vowel(text):
+    result = ""
+    vowels = "ieaouIEAOU"
+    for char in text:
+          if char not in vowels:
+            result += char
+    return result
+print anti_vowel("hello book")

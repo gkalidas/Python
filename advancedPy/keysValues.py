138ca54dadcae6f5ded12a5092c8036fcc57c1ee
Ganesh Londhe <ganeshlondhe2020@gmail.com>
2019-01-08 15:35:44 +0530

    returns keys and values respectively and randomly

---
+++ advancedPy/keysValues.py
@@ -0,0 +1,11 @@
+#The .keys() method returns a list of the dictionary's keys, and
+#The .values() method returns a list of the dictionary's values.
+
+
+my_dict = {
+  "Name":"Ganesh",
+  "Age": 23,
+  "Company": "PTC"
+}
+print my_dict.keys()
+print my_dict.values()

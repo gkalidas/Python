7867f1f187951239ad6c9534bbacc30be470ef49
Ganesh Londhe <ganeshlondhe2020@gmail.com>
2019-01-08 15:31:40 +0530

    iterates through dict and prints the elements randomly

---
+++ advancedPy/iteratorsDict.py
@@ -0,0 +1,10 @@
+#Create your own Python dictionary, my_dict, in the editor to the right with two or three key/value pairs.
+#Then, print the result of calling the my_dict.items().
+#Note that the .items() method doesn't return key/value pairs in any specific order.
+
+my_dict = {
+  "Name":"Ganesh",
+  "Age": 23,
+  "Company": "PTC"
+}
+print my_dict.items()

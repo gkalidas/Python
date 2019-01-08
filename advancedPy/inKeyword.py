1350c77162f763a44f53c7bc6498e6cab75f2261
Ganesh Londhe <ganeshlondhe2020@gmail.com>
2019-01-08 15:58:21 +0530

    use of in keyword

---
+++ advancedPy/inKeyword.py
@@ -0,0 +1,10 @@
+#For each key in my_dict: print out the key , then a space, then the value stored by that key.
+#(You should use print a, b rather than print a + " " + b.)
+
+my_dict = {
+  "Name":"Ganesh",
+  "Age": 23,
+  "Company": "PTC"
+}
+for x in my_dict:
+  print x, my_dict[x]

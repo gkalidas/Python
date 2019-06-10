5985cd1c2c364dbcedbd9ef3122c7a7c50cf2db9
Ganesh Londhe <ganeshlondhe2020@gmail.com>
2019-06-10 16:00:51 +0530

    OS file utilities like modification time, file exists, path

---
+++ practice/Lynda_Python/Ch4/ospathutils_start.py
@@ -0,0 +1,43 @@
+#
+# Example file for working with os.path module
+#
+import os
+from os import path
+import datetime
+from datetime import date, time, timedelta
+import time
+
+
+def main():
+  # Print the name of the OS
+  print(os.name)
+
+  # # Check for item existence and type
+  # print("Item exists: "+ str(path.exists("textfile.txt")))
+  # print("Item is a file: "+ str(path.isfile("textfile.txt")))
+  # print("Item is a directory: "+ str(path.isdir("textfile.txt")))
+  
+  # Work with file paths
+  # print("Item  path: "+str(path.realpath("textfile.txt")))
+  # #to separate file name from path
+  # print("Item path and name: "+str(path.split(path.realpath("textfile.txt"))))
+  
+  # # Get the modification time
+  # #modification time of the file
+  # # o/p - Mon Jun 10 15:34:50 2019
+  # t = time.ctime(path.getmtime("textfile.txt"))
+  # print(t)
+  # #this will print the miliseconds
+  # # - 2019-06-10 15:34:50.478359
+  # print(datetime.datetime.fromtimestamp(path.getmtime("textfile.txt")))
+  
+  # Calculate how long ago the item was modified
+  td = datetime.datetime.now() - datetime.datetime.fromtimestamp(
+    path.getmtime("textfile.txt")
+  )
+  print("It has been "+str(td) + "since the file was modified")
+  print("Or, " + str(td.total_seconds())+ " seconds")
+
+  
+if __name__ == "__main__":
+  main()
